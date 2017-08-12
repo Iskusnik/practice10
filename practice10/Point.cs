21d84c3807af4a338c7769112dd4868f9770eeaa
IskusnikXD <IskusnikXD@ISKUSNIK>
2017-08-12 12:57:21 +0500

    Незначительно переделан список из задания 9

---
+++ practice10/Point.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice10
+{
+    class Point
+    {
+        public int Info { get; set; }
+        public Point Next { get; set; }
+        public Point Previous { get; set; }
+        public Point(int info)
+        {
+            Info = info;
+            Next = null;
+        }
+        public Point(int info, Point prev)
+        {
+            Info = info;
+            Next = null;
+            Previous = prev
+        }
+        public static implicit operator bool(Point point)
+        {
+            if (point.Info >= 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
--- practice10/Program.cs
+++ practice10/Program.cs
@@ -8,6 +8,13 @@ namespace practice10
 {
     class Program
     {
+        /// <summary>
+        /// 14.	Задание 533в из книги 
+        /// Абрамов С.А., Гнездилова Г.Г., Капустина Е.Н., Селюн М.И. Задачи по программированию. М.: Наука, 1988.
+        /// На входе: n, X1, X2, ..., Xn
+        /// На выход: (X1 + X2 + 2*Xn)(X2 + X3 + 2*Xn-1)...(Xn-1 + Xn + 2*X2)
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
         }
